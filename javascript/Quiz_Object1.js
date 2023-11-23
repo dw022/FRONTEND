b60329bf381fe6bda824fee2c255598470029c88
dw022 <acebasket@naver.com>
2023-11-23 19:49:18 +0900

    Add Quiz_Object1.js

--- javascript/Quiz_Object1.js
+++ javascript/Quiz_Object1.js
@@ -1,3 +1,4 @@
+//korea 객체 추가
 var korea = {
     capital: "서울",
     population: 1232321,
@@ -10,3 +11,14 @@ var korea = {
 
 console.log(korea.isG7);
 console.log(korea.kpop());
+
+//기네스가 재입고되었습니다. 판매여부를 업데이트 해보세요.
+var irishBeer = {
+    name: "기네스",
+    origin: "아일랜드",
+    available: false // 판매여부
+}
+
+irishBeer.available = true;
+
+console.log(irishBeer.available);
