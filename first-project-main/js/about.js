05f3186a0599f0d3178a811a9b7700ef142ab3d5
leedawoon <acebasket42@gmail.com>
2024-03-09 17:18:01 +0900

    modified and debugging

--- first-project-main/js/about.js
+++ first-project-main/js/about.js
@@ -2,25 +2,30 @@ console.log("scripts for about page");
 
 document.getElementById("showmorebtn").addEventListener("click", function () {
     var additionalProfiles = document.querySelectorAll(".additional-profiles .additional");
-  
+
     event.preventDefault(); // 기본 동작 중지
-  
+
     // 추가적인 프로필 카드들 보이기/숨기기
     additionalProfiles.forEach(function(card) {
-      card.classList.toggle("hidden");
+        card.classList.toggle("hidden");
     });
-  
+
     // "Who am I?" 버튼 텍스트 변경
     var showMoreBtn = document.getElementById("showmorebtn");
-    if (showMoreBtn.textContent === "Who am I?") {
-      showMoreBtn.textContent = "▲";
-    } else {
-      showMoreBtn.textContent = "Who am I?";
-    }
-  });
-  
-  function toggleSubCard(button) {
-          var card = button.parentElement;
-          var subCard = card.querySelector('.sub-card');
-          subCard.classList.toggle('hidden');
-      }
+    showMoreBtn.textContent = (showMoreBtn.textContent === "Who am I?") ? "▲" : "Who am I?";
+});
+
+function toggleSubCard(button) {
+    var card = button.parentElement;
+    var subCard = card.querySelector('.sub-card');
+    subCard.classList.toggle('hidden');
+}
+
+const contactBtn = document.getElementById('contactbtn');
+const additionalProfile = document.querySelector('.contact-additional-profiles');
+const additionalProfileCard = document.querySelector('.contact-additional');
+
+contactBtn.addEventListener('click', () => {
+    additionalProfileCard.classList.toggle('hidden');
+    additionalProfile.classList.toggle('hidden');
+});
